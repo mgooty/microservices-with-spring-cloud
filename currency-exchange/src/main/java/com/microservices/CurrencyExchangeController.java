33664ea3f850c29dabbd2f1470cd24ad64426ac9
Mithun Gooty <mithun.gooty@gmail.com>
2023-11-22 17:34:07 +0530

    somemore changes

--- currency-exchange/src/main/java/com/microservices/CurrencyExchangeController.java
+++ currency-exchange/src/main/java/com/microservices/CurrencyExchangeController.java
@@ -1,6 +1,8 @@
 package com.microservices;
 
 import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 
     @Resource
     private Environment environment;
@@ -18,6 +22,7 @@ public class CurrencyExchangeController {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchangeResource getConversionMultiple(@PathVariable String from,
                                                         @PathVariable String to) {
+        logger.info("getConversionMultiple called with from {} to {}", from, to);
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 
         return new CurrencyExchangeResource(currencyExchange.getFrom(), currencyExchange.getTo(), currencyExchange.getConversionMultiple(), environment.getProperty("local.server.port"));
